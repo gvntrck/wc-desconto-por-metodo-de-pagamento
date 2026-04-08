ebcb70869f2f4c52052556ed2dc57dfe19f25837
GIOVANI TURECK <gvntrck@gmail.com>
2026-04-08 17:53:34 -0300

    feat:  plugin-update-checker

--- wc-ajuste-pagamento.php
+++ wc-ajuste-pagamento.php
@@ -16,6 +16,21 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/user-name/repo-name/',
+	__FILE__,
+	'unique-plugin-or-theme-slug'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('stable-branch-name');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('your-token-here');
+
 if (!class_exists('WC_Metodo_Pagamento_Acrescimo')) {
 	final class WC_Metodo_Pagamento_Acrescimo
 	{
